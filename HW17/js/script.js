8ade0ce91175bbde8ba6c60eb7302ccc39b15d49
Svetlana Nagovicyna <52661934+SvetlanaNagovicyna@users.noreply.github.com>
2022-01-27 00:36:58 +0200

    HW17 secundomer

--- HW17/js/script.js
+++ HW17/js/script.js
@@ -9,8 +9,54 @@
 */
 
 
-let timerInterval;
-let timer = 0;
+let milliseconds = 0;
+let timer;
+const time = document.querySelector('.time');
+const hours = document.querySelector('#hours');
 const minute = document.querySelector('#minute');
 const sec = document.querySelector('#sec');
 const ms = document.querySelector('#ms');
+
+
+const startTimer = () => {
+	time.classList.remove('paused');
+	clearInterval(timer);
+	timer = setInterval(() => {
+		milliseconds += 10;
+		let newTimer = new Date(milliseconds);
+		hours.innerHTML = ('0' + newTimer.getUTCHours()).slice(-2);
+		minute.innerHTML = ('0' + newTimer.getUTCMinutes()).slice(-2);
+		sec.innerHTML = ('0' + newTimer.getUTCSeconds()).slice(-2);
+		ms.innerHTML = ('0' + newTimer.getUTCMilliseconds()).slice(-3, -1);
+
+	}, 10);
+
+};
+
+const stopTimer = () => {
+	time.classList.add('stop');
+	clearInterval(timer);
+};
+
+const resetTimer = () => {
+	time.classList.remove('stop');
+	clearInterval(timer);
+	milliseconds = 0;
+	hours.innerHTML = '00';
+	minute.innerHTML = '00';
+	sec.innerHTML = '00';
+	ms.innerHTML = '00';
+};
+
+document.addEventListener('click', (e) => {
+	const elem = e.target;
+	if (elem.id === 'start') {
+		startTimer();
+	}
+	if (elem.id === 'stop') {
+		stopTimer();
+	}
+	if (elem.id === 'reset') {
+		resetTimer();
+	}
+})
