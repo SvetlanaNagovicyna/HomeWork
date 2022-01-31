8d3f4e077fdd9c330fa856f4e4d94e493420a52b
Nagovicyna Svetlana <nagovicynasveta@clickable.agency>
2022-01-31 15:43:50 +0200

    edit HW18

--- HW18/js/script.js
+++ HW18/js/script.js
@@ -8,11 +8,48 @@
 
 */
 
-const btns = document.querySelectorAll('.increment, .decrement')
+// const btns = document.querySelectorAll('.increment, .decrement')
 
-btns.forEach(function (btn) {
-	btn.addEventListener('click', function () {
-		let count = this.closest('.square').querySelector('.count')
-		this.classList.contains('increment') ? count.textContent++ : count.textContent--;
-	})
+// btns.forEach(function (btn) {
+// 	btn.addEventListener('click', function () {
+// 		let count = this.closest('.square').querySelector('.count')
+// 		this.classList.contains('increment') ? count.textContent++ : count.textContent--;
+// 	})
+// })
+
+
+const wrap = document.querySelector('.wrapper')
+
+wrap.addEventListener('click', function (event) {
+	const target = event.target;
+	const count = target.closest('.square').querySelector('.count')
+
+	if (target.classList.contains('increment')) {
+		count.textContent++
+	} else if (target.classList.contains('decrement')) {
+		count.textContent--
+	}
 })
+
+
+
+
+let add = document.querySelector('.add')
+add.addEventListener('click', function () {
+	let wrapper = document.querySelector('.wrapper');
+
+	let newDiv = document.createElement('div');
+	newDiv.classList.add('square');
+	newDiv.innerHTML = `<div class="count">0</div>
+						<div class="btns">
+							<button class="decrement">-</button>
+							<button class="increment">+</button>
+						</div>`
+	wrapper.appendChild(newDiv);
+	console.dir(newDiv);
+	// console.log(btns);
+
+
+})
+
+// console.log(btns);
