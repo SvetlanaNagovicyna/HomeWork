90a7e4cf313bd2fb11e5120902ea960203de4d4a
Nagovicyna Svetlana <nagovicynasveta@clickable.agency>
2022-02-01 14:35:48 +0200

    add in HW20

--- HW20/js/script.js
+++ HW20/js/script.js
@@ -6,20 +6,33 @@
 
 
 
-document.querySelectorAll('.popup-btn').forEach(btn => {
+// document.querySelectorAll('.popup-btn').forEach(btn => {
+// 	btn.addEventListener('click', function () {
+// 		const popupBlock = document.querySelector('#' + btn.dataset.popup);
+// 		popupBlock.classList.add('open');
+// 	})
+// });
+
+// document.querySelectorAll('.popup-wrap').forEach(wrap => {
+// 	wrap.addEventListener('click', event => {
+// 		const eventTarg = event.target;
+// 		if (eventTarg.closest('.close')) {
+// 			wrap.classList.remove('open')
+// 		}
+// 	})
+// })
+
+document.querySelectorAll('.btn').forEach(btn => {
 	btn.addEventListener('click', function () {
-		const popupBlock = document.querySelector('#' + btn.dataset.popup);
+		const popupBlock = document.querySelector('.rules');
 		popupBlock.classList.add('open');
 	})
 });
 
 document.querySelectorAll('.popup-wrap').forEach(wrap => {
 	wrap.addEventListener('click', event => {
-
 		const eventTarg = event.target;
-		console.log(eventTarg);
-
-		if (eventTarg.classList.contains('close') || eventTarg.closest('.close')) {
+		if (eventTarg.closest('.close')) {
 			wrap.classList.remove('open')
 		}
 	})
