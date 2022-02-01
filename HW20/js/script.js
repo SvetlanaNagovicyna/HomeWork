27d2cd27b4234c2df8a311c03bc1245f4baf1f82
Nagovicyna Svetlana <nagovicynasveta@clickable.agency>
2022-02-01 17:14:30 +0200

    HW20

--- HW20/js/script.js
+++ HW20/js/script.js
@@ -6,52 +6,52 @@
 
 
 
-// document.querySelectorAll('.popup-btn').forEach(btn => {
-// 	btn.addEventListener('click', function () {
-// 		const popupBlock = document.querySelector('#' + btn.dataset.popup);
-// 		popupBlock.classList.add('open');
-// 	})
-// });
-
-// document.querySelectorAll('.popup-wrap').forEach(wrap => {
-// 	wrap.addEventListener('click', event => {
-// 		const eventTarg = event.target;
-// 		console.log(eventTarg);
-
-// 		if (eventTarg.closest('.close') || eventTarg === wrap) {
-// 			wrap.classList.remove('open')
-// 		}
-// 	})
-// })
-
-
-
-
-document.querySelectorAll('.popup2').forEach(btn => {
+document.querySelectorAll('.popup-btn').forEach(btn => {
 	btn.addEventListener('click', function () {
-		const popupBlock = document.querySelector('.popup-2');
+		const popupBlock = document.querySelector('#' + btn.dataset.popup);
 		popupBlock.classList.add('open');
 	})
 });
-
-
-
-
-document.querySelectorAll('.btn').forEach(btn => {
-	btn.addEventListener('click', function () {
-		const popupBlock = document.querySelector('.rules');
-		popupBlock.classList.add('open');
-	})
-});
-
-
-
 
 document.querySelectorAll('.popup-wrap').forEach(wrap => {
 	wrap.addEventListener('click', event => {
 		const eventTarg = event.target;
-		if (eventTarg.closest('.close')) {
+		console.log(eventTarg);
+
+		if (eventTarg.closest('.close') || eventTarg === wrap) {
 			wrap.classList.remove('open')
 		}
 	})
 })
+
+
+
+
+// document.querySelectorAll('.popup2').forEach(btn => {
+// 	btn.addEventListener('click', function () {
+// 		const popupBlock = document.querySelector('.popup-2');
+// 		popupBlock.classList.add('open');
+// 	})
+// });
+
+
+
+
+// document.querySelectorAll('.btn').forEach(btn => {
+// 	btn.addEventListener('click', function () {
+// 		const popupBlock = document.querySelector('.rules');
+// 		popupBlock.classList.add('open');
+// 	})
+// });
+
+
+
+
+// document.querySelectorAll('.popup-wrap').forEach(wrap => {
+// 	wrap.addEventListener('click', event => {
+// 		const eventTarg = event.target;
+// 		if (eventTarg.closest('.close')) {
+// 			wrap.classList.remove('open')
+// 		}
+// 	})
+// })
