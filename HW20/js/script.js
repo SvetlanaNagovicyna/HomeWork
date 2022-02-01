eb1c1130f91cd746f2a0c82bddf190a317bd9cb4
Nagovicyna Svetlana <nagovicynasveta@clickable.agency>
2022-02-01 15:53:29 +0200

    HW20

--- HW20/js/script.js
+++ HW20/js/script.js
@@ -22,12 +22,25 @@
 // 	})
 // })
 
+document.querySelectorAll('.popup2').forEach(btn => {
+	btn.addEventListener('click', function () {
+		const popupBlock = document.querySelector('.popup-2');
+		popupBlock.classList.add('open');
+	})
+});
+
+
+
+
 document.querySelectorAll('.btn').forEach(btn => {
 	btn.addEventListener('click', function () {
 		const popupBlock = document.querySelector('.rules');
 		popupBlock.classList.add('open');
 	})
 });
+
+
+
 
 document.querySelectorAll('.popup-wrap').forEach(wrap => {
 	wrap.addEventListener('click', event => {
