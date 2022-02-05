18109fc9bd4804d3115d1585f90ab183831bdc3e
Svetlana Nagovicyna <52661934+SvetlanaNagovicyna@users.noreply.github.com>
2022-02-05 16:59:20 +0200

    add HW21

---
+++ HW21/js/script.js
@@ -0,0 +1,104 @@
+/* 
+
+ДЗ 21. Реализовать map, filter, every, some
+
+* reduce
+
+*/
+
+
+// map
+
+function map(arr, fn) {
+
+	let newArr = [];
+
+	for (let i = 0; i < arr.length; i++) {
+
+		newArr[i] = fn(arr[i]);
+	}
+
+	return newArr;
+}
+
+let fnMap = map([23, 4, 2, 34, 24, 2], function (item) {
+	return item * 2;
+})
+
+console.log(fnMap);
+
+
+// // filter
+
+
+function filter(arr, fn) {
+
+	let newArr = [];
+
+	for (let i = 0; i < arr.length; i++) {
+
+
+		if (fn(arr[i])) {
+
+			newArr.push(arr[i]);
+		}
+
+	}
+	return newArr;
+}
+
+let fnFilter = filter([23, 4, 2, 34, 24, 2], function (item) {
+	return item > 4;
+})
+
+console.log(fnFilter);
+
+
+// every
+
+function every(arr, fn) {
+
+	let element;
+
+	for (let i = 0; i < arr.length; i++) {
+		element = fn(arr[i]);
+	}
+
+	if (element === true) {
+		return true
+	} else {
+		return false
+	}
+
+}
+
+let fnEvery = every([23, 4, 2, 34, 24, 2], function (item) {
+	return item >= 4;
+})
+
+console.log(fnEvery);
+
+
+// some
+
+function some(arr, fn) {
+
+	let element;
+
+	for (let i = 0; i < arr.length; i++) {
+		element = fn(arr[i]);
+	}
+
+	if (element === true) {
+		return true
+	} else {
+		return false
+	}
+
+}
+
+let fnSome = some([23, 4, 2, 34, 24, 2], function (item) {
+	return item >= 4;
+})
+
+console.log(fnSome);
