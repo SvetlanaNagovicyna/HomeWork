af3b513be0297a0442a7de09f00bbd4e36c20a9d
Nagovicyna Svetlana <nagovicynasveta@clickable.agency>
2022-02-07 18:01:25 +0200

    add

--- HW21/js/script.js
+++ HW21/js/script.js
@@ -9,111 +9,140 @@
 
 // map
 
-function map(arr, fn) {
+// function map(arr, fn) {
 
-	let newArr = [];
+// 	let newArr = [];
 
-	for (let i = 0; i < arr.length; i++) {
+// 	for (let i = 0; i < arr.length; i++) {
 
-		newArr[i] = fn(arr[i], i, arr);
-	}
+// 		newArr[i] = fn(arr[i], i, arr);
+// 	}
 
-	return newArr;
-}
+// 	return newArr;
+// }
 
-let fnMap = map([23, 4, 2, 34, 24, 2], function (item, i, arr) {
-	return item * 2;
-})
+// let fnMap = map([23, 4, 2, 34, 24, 2], function (item, i, arr) {
+// 	return item * 2;
+// })
 
-console.log(fnMap);
-
-
-// // filter
+// console.log(fnMap);
 
 
-function filter(arr, fn) {
-
-	let newArr = [];
-
-	for (let i = 0; i < arr.length; i++) {
+// // // filter
 
 
-		if (fn(arr[i], i, arr)) {
+// function filter(arr, fn) {
 
-			newArr.push(arr[i]);
-		}
+// 	let newArr = [];
 
-	}
-	return newArr;
-}
+// 	for (let i = 0; i < arr.length; i++) {
 
-let fnFilter = filter([23, 4, 2, 34, 24, 2], function (item) {
-	return item > 4;
-})
 
-console.log(fnFilter);
+// 		if (fn(arr[i], i, arr)) {
+
+// 			newArr.push(arr[i]);
+// 		}
+
+// 	}
+// 	return newArr;
+// }
+
+// let fnFilter = filter([23, 4, 2, 34, 24, 2], function (item) {
+// 	return item > 4;
+// })
+
+// console.log(fnFilter);
 
 
 // every
 
 function every(arr, fn) {
 
-	let element;
-
 	for (let i = 0; i < arr.length; i++) {
-		element = fn(arr[i], i, arr);
-	}
 
-	if (element === true) {
-		return true
-	} else {
-		return false
+		if (!fn(arr[i], i, arr)) {
+			return false
+		}
 	}
+	return true;
 
 }
 
-let fnEvery = every([23, 4, 2, 34, 24, 2], function (item) {
-	return item >= 4;
+let fnEvery = every([23, 4, 2, 34, 24, 3], function (item) {
+	return item >= 3;
 })
 
-console.log(fnEvery);
+// console.log(fnEvery);
 
 
 // some
 
 function some(arr, fn) {
 
-	let element;
-
 	for (let i = 0; i < arr.length; i++) {
-		element = fn(arr[i], i, arr);
 
-		if (element === true) {
+		if (fn(arr[i], i, arr)) {
 			return true
-		} else {
-			return false
 		}
 	}
+	return false
 
 }
 
 let fnSome = some([23, 4, 2, 34, 24, 2], function (item) {
-	return item >= 4;
+	return item === 40;
 })
 
-console.log(fnSome);
+// console.log(fnSome);
 
 
 // reduce
 
-function reduce(arr, acc) {
+function reduce(arr, fn, acc) {
 
 	for (let i = 0; i < arr.length; i++) {
-		acc += arr[i];
+		acc += fn(acc, arr[i]);
 	}
 	return acc;
 }
 
-let fnReduce = reduce([23, 4, 2, 34, 24, 2], 0);
+let fnReduce = reduce([23, 4, 2, 34, 24, 2], function (acc, currentValue, index, array) {
+	return acc += currentValue;
+}, 0);
 
-console.log(fnReduce);
+// console.log(fnReduce);
+
+
+
+//reduce 
+
+
+// const array = [24, 4, 2, 34, 24, 2];
+
+
+// let re = array.reduce(function (previousValue, currentValue, index) {
+
+// 	previousValue[index] = currentValue;
+
+// 	return previousValue;
+
+// }, {})
+
+// console.log(re)
+
+
+
+function reduce(arr, fn, acc) {
+
+	for (let i = 0; i < arr.length; i++) {
+		acc += fn(acc, arr[i]);
+	}
+	return acc;
+}
+
+let fnReduce = reduce([23, 4, 2, 34, 24, 2], function (previousValue, currentValue, index, array) {
+	previousValue[index] = currentValue;
+	return previousValue;
+}, 0);
+
+// console.log(fnReduce);
