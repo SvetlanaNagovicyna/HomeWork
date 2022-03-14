395acfacdfe51dfb3ba5407a5337afd29df83a2c
Svetlana Nagovicyna <52661934+SvetlanaNagovicyna@users.noreply.github.com>
2022-03-14 23:27:00 +0200

    add HW26

---
+++ HW26/js/script.js
@@ -0,0 +1,21 @@
+/* 
+
+ДЗ 26. На странице есть изображение.
+
+Сделать, чтобы при наведении указателя мыши на изображение это изображение переворачивалось вверх ногами
+
+Не использовать hover, сделать на событиях js
+*/
+
+
+
+const $img = document.querySelector('.image img');
+
+
+$img.addEventListener('mousemove', e => {
+	$img.style.transform = 'rotate(180deg)';
+})
+
+$img.addEventListener('mouseout', e => {
+	$img.style.transform = 'rotate(0deg)';
+})
