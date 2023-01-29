de2645abf3036a20c13daa37a6cd4c35ba5d8f4a
Svetlana Nagovicyna <52661934+SvetlanaNagovicyna@users.noreply.github.com>
2023-01-30 00:05:01 +0200

    add hw search

---
+++ HW38/js/script.js
@@ -0,0 +1,28 @@
+/* 
+
+ДЗ 38. Есть список с десятком элементов, над ним находится инпут, в него можно вписать текст, по мере ввода в списке должны оставаться только пункты которые содержат то что ввели, остальные скрываются
+Большие/маленькие буквы игнорируются
+
+*/
+
+
+const $input = document.querySelector('input');
+const $list = document.querySelectorAll('li');
+
+function findTxt() {
+	$list.forEach(el => {
+		el.textContent.toLowerCase().includes($input.value.toLowerCase()) ?
+			el.style.display = "block" :
+			el.style.display = "none";
+	})
+}
+
+findTxt();
+
+$input.addEventListener('input', () => {
+	findTxt();
+});
+
+$input.addEventListener('change', () => {
+	findTxt();
+});
